4d2e73382976c34dc11fac1dab81bf82738a2db9
Unicred do Brasil - Nicolas Campos <nicolas.campos@e-unicred.com.br>
2018-04-11 12:06:46 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/br/com/empresa/tcr/api/TcrApplication.java
@@ -0,0 +1,12 @@
+package br.com.empresa.tcr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcrApplication.class, args);
+	}
+}
