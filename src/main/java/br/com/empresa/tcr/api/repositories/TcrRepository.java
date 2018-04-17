25b952a3621c91074995e32322cf4e06fc5e0186
nicolas.campos <nicolas.campos@unicred.com.br>
2018-04-17 14:32:24 -0300

    Ajuste na nomenclatura dos métodos desenvolvidos.

--- src/main/java/br/com/empresa/tcr/api/repositories/TcrRepository.java
+++ src/main/java/br/com/empresa/tcr/api/repositories/TcrRepository.java
@@ -1,9 +1,18 @@
 package br.com.empresa.tcr.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.empresa.tcr.api.entities.Tcr;
 
+@Transactional(readOnly = true)
 public interface TcrRepository extends JpaRepository<Tcr, Long> {
-
+	
+	List<Tcr> findByDsNomeAndCdCooperativa(String dsNome, Integer cdCooperativa);
+	
+	List<Tcr> findByCdCooperativaAndCdPosto(Integer cdCooperativa, Integer cdPosto);
+	
+	List<Tcr> findByDsNomeAndCdCooperativaAndCdPosto(String dsNome, Integer cdCooperativa, Integer cdPosto);
 }
