bb6d2fa5e014f5142b62b3d0105ab56e5917e7f4
nicolas.campos <nicolas.campos@unicred.com.br>
2018-04-18 10:44:54 -0300

    Incçusão das classes de teste dos services de tcr.

--- src/test/java/br/com/empresa/tcr/api/repositories/TcrRepositoryTest.java
+++ src/test/java/br/com/empresa/tcr/api/repositories/TcrRepositoryTest.java
@@ -21,6 +21,7 @@ import br.com.empresa.tcr.api.entities.Retirada;
 import br.com.empresa.tcr.api.entities.Saque;
 import br.com.empresa.tcr.api.entities.Suprimento;
 import br.com.empresa.tcr.api.entities.Tcr;
+import br.com.empresa.tcr.api.utils.TcrUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,37 +34,11 @@ public class TcrRepositoryTest {
 	private static final Integer    COOPERATIVA 	   = 515;
 	private static final Integer    POSTO 			   = 0;
 	private static final String     NOME_TESOUREIRO    = "teste";
-	private static final String     USUARIO		 	   = "teste";
-	private static final Character  NAO			 	   = 'N';
-	private static final Character  SIM			 	   = 'S';
-	private static final BigDecimal VALOR_MAX_DIARIO   = new BigDecimal(100);
-	private static final BigDecimal VALOR_MAX_OPERACAO = new BigDecimal(100);
-	private static final Date 		DATA			   = new Date();
+
 	
 	@Before
 	public void setUp() throws Exception {
-		
-		Deposito   deposito   = obterDadosDeposito();
-		Retirada   retirada   = obterDadosRetirada();
-		Saque 	   saque 	  = obterDadosSaque();
-		Suprimento suprimento = obterDadosSuprimento();
-		
-		Tcr tcr = new Tcr();
-		
-		tcr.setDsNome(NOME_TESOUREIRO);
-		tcr.setCdPosto(POSTO);
-		tcr.setCdCooperativa(COOPERATIVA);
-		tcr.setIdTesoureiroAtivo(NAO);
-		tcr.setCdUsuarioAlt(USUARIO);
-		tcr.setDtInclusao(DATA);
-		tcr.setDtAlteracao(DATA);
-		tcr.setIdLimiteExcedidoAutorizaTesoureiro(NAO);
-		tcr.setDeposito(deposito);
-		tcr.setRetirada(retirada);
-		tcr.setSaque(saque);
-		tcr.setSuprimento(suprimento);
-		
-		this.tcrRepository.save(tcr);
+		this.tcrRepository.save(TcrUtils.obterTcr());
 	}
 	
 	@After
@@ -90,57 +65,5 @@ public class TcrRepositoryTest {
 		List<Tcr> tcrList = this.tcrRepository.findByDsNomeAndCdCooperativaAndCdPosto(NOME_TESOUREIRO, COOPERATIVA, POSTO);
 		
 		assertEquals(NOME_TESOUREIRO, tcrList.get(0).getDsNome());
-	}
-	
-	private Deposito obterDadosDeposito() throws NoSuchAlgorithmException {
-		Deposito   deposito   = new Deposito();
-		deposito.setIdAtivo(SIM);
-		deposito.setVlMaxDiario(VALOR_MAX_DIARIO);
-		deposito.setVlMaxOperacao(VALOR_MAX_OPERACAO);
-		deposito.setCdUsuarioAlt(USUARIO);
-		deposito.setDtInclusao(DATA);
-		deposito.setDtAlteracao(DATA);
-		
-		return deposito;
-	}
-	
-	private Retirada obterDadosRetirada() throws NoSuchAlgorithmException {
-		Retirada   retirada   = new Retirada();
-		retirada.setIdAtivo(SIM);
-		retirada.setVlMaxDiario(VALOR_MAX_DIARIO);
-		retirada.setVlMaxOperacao(VALOR_MAX_OPERACAO);
-		retirada.setCdUsuarioAlt(USUARIO);
-		retirada.setDtInclusao(DATA);
-		retirada.setDtAlteracao(DATA);
-		
-		return retirada;
-	}
-	
-	private Saque obterDadosSaque() throws NoSuchAlgorithmException {
-		Saque saque = new Saque();
-		saque.setIdAtivo(SIM);
-		saque.setVlMaxDiario(VALOR_MAX_DIARIO);
-		saque.setVlMaxOperacao(VALOR_MAX_OPERACAO);
-		saque.setIdExibeInventario(NAO);
-		saque.setIdBalanceamentoCedulas(NAO);
-		saque.setCdUsuarioAlt(USUARIO);
-		saque.setDtInclusao(DATA);
-		saque.setDtAlteracao(DATA);
-		
-		return saque;
-	}
-	
-	private Suprimento obterDadosSuprimento() throws NoSuchAlgorithmException {
-		Suprimento suprimento = new Suprimento();
-		suprimento.setIdAtivo(SIM);
-		suprimento.setVlMaxDiario(VALOR_MAX_DIARIO);
-		suprimento.setVlMaxOperacao(VALOR_MAX_OPERACAO);
-		suprimento.setIdExibeInventario(NAO);
-		suprimento.setIdBalanceamentoCedulas(NAO);
-		suprimento.setCdUsuarioAlt(USUARIO);
-		suprimento.setDtInclusao(DATA);
-		suprimento.setDtAlteracao(DATA);
-		
-		return suprimento;
 	}
 }
---
+++ src/test/java/br/com/empresa/tcr/api/services/TcrServiceTest.java
@@ -0,0 +1,80 @@
+package br.com.empresa.tcr.api.services;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.empresa.tcr.api.entities.Tcr;
+import br.com.empresa.tcr.api.repositories.TcrRepository;
+import br.com.empresa.tcr.api.utils.TcrUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class TcrServiceTest {
+
+	@MockBean
+	private TcrRepository tcrRepository;
+	
+	@Autowired
+	TcrService tcrService;
+	
+	private static final String  NOME 		 = "teste";
+	private static final Integer COOPERATIVA = 515;
+	private static final Integer POSTO 		 = 0;
+	
+	@Before
+	public void setUp() throws Exception {
+		List<Tcr> list = new ArrayList<Tcr>();
+		list.add(new Tcr());
+		
+		BDDMockito.given(this.tcrRepository.findByDsNomeAndCdCooperativa(Mockito.anyString(), Mockito.anyInt())).willReturn(list);
+		BDDMockito.given(this.tcrRepository.findByCdCooperativaAndCdPosto(Mockito.anyInt(), Mockito.anyInt())).willReturn(list);
+		BDDMockito.given(this.tcrRepository.findByDsNomeAndCdCooperativaAndCdPosto(Mockito.anyString(), Mockito.anyInt(), Mockito.anyInt())).willReturn(list);
+		BDDMockito.given(this.tcrRepository.save(Mockito.any(Tcr.class))).willReturn(new Tcr());
+	}
+	
+	@Test
+	public void testBuscarTcrPorNomeECooperativa() {
+		Optional<Tcr> tcrOpt = Optional.ofNullable(this.tcrService.buscarPorNomeECooperativa(NOME, COOPERATIVA).get(0));
+		
+		assertTrue(tcrOpt.isPresent());
+	}
+	
+	@Test
+	public void testBuscarPorCooeprativaEPosto() {
+		Optional<Tcr> tcrOpt = Optional.ofNullable(this.tcrService.buscarPorCooperativaEPosto(COOPERATIVA, POSTO).get(0));
+		
+		assertTrue(tcrOpt.isPresent());
+	}
+	
+	@Test
+	public void testBuscarPorNomeCooperativaEPosto() {
+		Optional<Tcr> tcrOpt = Optional.ofNullable(this.tcrService.buscarPorNomeCooperativaEPosto(NOME, COOPERATIVA, POSTO).get(0));
+		
+		assertTrue(tcrOpt.isPresent());
+	}
+	
+	@Test
+	public void testPersistirTcr() throws Exception {
+		Tcr tcr = this.tcrService.persistir(TcrUtils.obterTcr());
+		
+		assertNotNull(tcr);
+	}
+	
+	
+}
