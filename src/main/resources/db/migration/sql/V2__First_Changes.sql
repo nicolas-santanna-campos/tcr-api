d81536ef5689cf10c92be9688d0b71a658bbe51d
nicolas.campos <nicolas.campos@unicred.com.br>
2018-04-23 08:55:11 -0300

    Alterações no versionamento de banco de dados, exclusão de colunas.

---
+++ src/main/resources/db/migration/sql/V2__First_Changes.sql
@@ -0,0 +1,35 @@
+ALTER TABLE suprimento
+DROP COLUMN cd_usuario_alt;
+
+ALTER TABLE suprimento
+DROP COLUMN dt_inclusao;
+
+ALTER TABLE suprimento
+DROP COLUMN dt_alteracao;
+
+ALTER TABLE saque
+DROP COLUMN cd_usuario_alt;
+
+ALTER TABLE saque
+DROP COLUMN dt_inclusao;
+
+ALTER TABLE saque
+DROP COLUMN dt_alteracao;
+
+ALTER TABLE deposito
+DROP COLUMN cd_usuario_alt;
+
+ALTER TABLE deposito
+DROP COLUMN dt_inclusao;
+
+ALTER TABLE deposito
+DROP COLUMN dt_alteracao;
+
+ALTER TABLE retirada
+DROP COLUMN cd_usuario_alt;
+
+ALTER TABLE retirada
+DROP COLUMN dt_inclusao;
+
+ALTER TABLE retirada
+DROP COLUMN dt_alteracao;
